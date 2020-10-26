91a9def6971e3455e9feccb25c3ad25b75786594
pasupula priyanka <pasupulapriyanka123@gmail.com>
2020-10-26 19:14:05 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import React,{useEffect,useState} from 'react'
 import './App.css';
-
+import axios from 'axios';
 function App() {
+  const [data,setData] = useState();
+   useEffect(()=>{
+    axios.get("https://corona.lmao.ninja/v2/all")
+    .then(response=>{
+      setData(response.data)
+      console.log(response.data)
+    })
+   },[]);
+
+   
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>COVID TRACKER</h1>
+        <hr></hr>
+        <h1 className="cases"> Cases: {data.cases}</h1>
+        <h1 className="recovered">Recovered: {data.recovered}</h1>
+        <h1 className="critical">Critical: {data.critical}</h1>
+        <h1>Deaths: {data.deaths}</h1>
     </div>
   );
 }
